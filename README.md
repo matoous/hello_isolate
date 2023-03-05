62db15863c75dedfa5593a185e59a92890358bdd
Matouš Dzivjak <matousdzivjak@gmail.com>
2023-03-05 22:13:44 +0100

    feat: V8 dispose

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Application that prints `Hello World!` from a JavaScript script
 running in [V8 Isolate](https://v8docs.nodesource.com/node-0.8/d5/dda/classv8_1_1_isolate.html)
 in [Rust](https://www.rust-lang.org/).
 
+This repository contains the code for the [How do CloudFlare Workers work?](https://dzx.cz/2023/03/08/how_do_cloudflare_workers_work/)
+blog post.
+
+The `main` branch has simple example for _Hello World!_.
+You can find slightly more complex example that compiles the script as a worker
+and provides basic runtime in the [hello-runtime](https://github.com/matoous/hello_isolate/tree/hello-runtime) branch.
+
 ## Run
 
 Run using
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,24 @@ fn main() {
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 
-    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+    {
+        let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
 
-    let handle_scope = &mut v8::HandleScope::new(isolate);
-    let context = v8::Context::new(handle_scope);
-    let scope = &mut v8::ContextScope::new(handle_scope, context);
+        let handle_scope = &mut v8::HandleScope::new(isolate);
+        let context = v8::Context::new(handle_scope);
+        let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-    let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
-    let script = v8::Script::compile(scope, code, None).unwrap();
+        let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+        let script = v8::Script::compile(scope, code, None).unwrap();
 
-    let result = script.run(scope).unwrap();
+        let result = script.run(scope).unwrap();
 
-    let result = result.to_string(scope).unwrap();
-    println!("{}", result.to_rust_string_lossy(scope));
+        let result = result.to_string(scope).unwrap();
+        println!("{}", result.to_rust_string_lossy(scope));
+    }
+
+    unsafe {
+        v8::V8::dispose();
+    }
+    v8::V8::dispose_platform();
 }
